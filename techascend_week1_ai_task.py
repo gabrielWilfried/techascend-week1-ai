d90df937a42e19ee936e800c479d365ea1672ee8
gabrielWilfried <gabrielwilfried0808@gmail.com>
2026-03-06 20:14:38 +0100

    print number 1 to 10 using for loop and while loop

--- techascend_week1_ai_task.py
+++ techascend_week1_ai_task.py
@@ -12,3 +12,16 @@ age: int = 25
 name: str = "Gabriel"
 temperature: float = 36.9
 
+# ─────────────────────────────────────────────
+# 2. LOOPS
+# ─────────────────────────────────────────────
+
+print("Print numbers from 1 to 10 using a for loop")
+for number in range(1, 11):
+    print(number)
+
+print("Print numbers from 1 to 10 using a while loop")
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
